a06bd80e7b1539ea201288cc38960b957857218e
Remco Kroonen <99039459@mydavinci.nl>
2019-04-01 23:30:43 +0200

    10 begin

---
+++ section-10/isbn-process.php
@@ -0,0 +1,32 @@
+<?php
+
+//check if form is there
+if (isset($_POST['submit'])) {
+	# code...
+
+
+//assign post variables
+	$isbn = $_POST['isbn'];
+
+//total incrementor
+	$total = 0;
+	$i = 1;
+
+//split the string
+	$digits = str_split($isbn);
+//loop through array
+	foreach ($digits as $digit) {
+		//update total = digit * 1, 2, 3, ect
+		$total += $digit * $i;
+		$i++;
+	}
+
+
+//check if divisible by 11
+if ($total % 11 == 0) {
+	echo "valid";
+} else {
+	echo "invalid";
+}
+
+}
